8d3a945df8a2860dde3a03a664ae18826e40941f
OysterQAQ <oysterqaq@gmail.com>
2019-09-24 09:44:57 +0800

    fixed: auth check exception
    权限校验bug

--- src/main/java/dev/cheerfun/pixivic/auth/exception/AuthLevelException.java
+++ src/main/java/dev/cheerfun/pixivic/auth/exception/AuthLevelException.java
@@ -2,6 +2,7 @@ package dev.cheerfun.pixivic.auth.exception;
 
 import dev.cheerfun.pixivic.common.exception.BaseException;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
  * @date 2019/07/18 14:07
  * @description 权限不足异常
  */
+@Data
 @AllArgsConstructor
 public class AuthLevelException extends BaseException {
     private HttpStatus httpStatus;
