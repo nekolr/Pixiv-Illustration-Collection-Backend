2ef995b512eea80adf83610fd2f2def242e89741
OysterQAQ <oysterqaq@gmail.com>
2019-12-14 20:48:25 +0800

    fixed: search keyword sensitive check
    搜索关键词过滤敏感词

---
+++ src/main/java/dev/cheerfun/pixivic/biz/notify/constant/NotifyActionType.java
@@ -0,0 +1,12 @@
+package dev.cheerfun.pixivic.biz.notify.constant;
+
+/**
+ * @author OysterQAQ
+ * @version 1.0
+ * @date 19-12-14 下午2:02
+ * @description NotifyActionType
+ */
+public class NotifyActionType {
+    public final static String COMMENT="comment";
+    public final static String LIKE="like";
+}
