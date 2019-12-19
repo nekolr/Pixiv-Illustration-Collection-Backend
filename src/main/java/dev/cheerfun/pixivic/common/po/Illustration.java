36769800a55dc47700b5188f15d2474fd33a63bf
OysterQAQ <oysterqaq@gmail.com>
2019-12-19 19:18:13 +0800

    fixed: search limit
    限制分页和页码

--- src/main/java/dev/cheerfun/pixivic/common/po/Illustration.java
+++ src/main/java/dev/cheerfun/pixivic/common/po/Illustration.java
@@ -1,6 +1,5 @@
 package dev.cheerfun.pixivic.common.po;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import dev.cheerfun.pixivic.common.po.illust.ArtistPreView;
 import dev.cheerfun.pixivic.common.po.illust.ImageUrl;
@@ -17,7 +16,7 @@ import java.util.List;
  * @description illustration
  */
 @Data
-public class Illustration {
+public class Illustration  {
     @JsonSetter("illust_id")
     private Integer id;
     @JsonSetter("artist_id")
@@ -29,29 +28,22 @@ public class Illustration {
     private ArtistPreView artistPreView;
     private List<Tag> tags;
     @JsonSetter("image_urls")
-    @JsonProperty("imageUrls")
     private List<ImageUrl> imageUrls;
     private List<String> tools;
     @JsonSetter("create_date")
-    @JsonProperty("createDate")
     private Date createDate;
     @JsonSetter("page_count")
-    @JsonProperty("pageCount")
     private Integer pageCount;
     private Integer width;
     private Integer height;
     @JsonSetter("sanity_level")
-    @JsonProperty("sanityLevel")
     private Integer sanityLevel;
     private Integer restrict;
     @JsonSetter("x_restrict")
-    @JsonProperty("xRestrict")
     private Integer xRestrict;
     @JsonSetter("total_view")
-    @JsonProperty("totalView")
     private Integer totalView;
     @JsonSetter("total_bookmarks")
-    @JsonProperty("totalBookmarks")
     private Integer totalBookmarks;
 
     public void setArtistPreView(Integer id, String name, String account, String avatar) {
