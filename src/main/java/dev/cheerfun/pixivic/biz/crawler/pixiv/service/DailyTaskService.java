8c570ffb48effccbfac2521f4b76a8cc9bfea41b
OysterQAQ <oysterqaq@gmail.com>
2020-01-06 10:27:06 +0800

    refactor: update crawler period
    更新爬虫周期

--- src/main/java/dev/cheerfun/pixivic/biz/crawler/pixiv/service/DailyTaskService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/crawler/pixiv/service/DailyTaskService.java
@@ -28,7 +28,7 @@ public class DailyTaskService {
         rankService.pullAllRank();
     }
 
-    @Scheduled(cron = "0 10 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 * * ?")
+    @Scheduled(cron = "0 10 * * * ?")
     public void mainCrawler() throws InterruptedException {
         mainCrawlerService.dailyPersistentTask();
     }
