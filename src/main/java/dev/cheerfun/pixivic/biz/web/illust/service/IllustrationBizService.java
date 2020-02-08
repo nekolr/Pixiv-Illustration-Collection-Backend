ac51c91db2e4f7f18af558ad8e8d445c8ab59a7e
OysterQAQ <oysterqaq@gmail.com>
2020-02-09 00:04:49 +0800

    fixed: related Illust
    关联画作不稳定问题

--- src/main/java/dev/cheerfun/pixivic/biz/web/illust/service/IllustrationBizService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/illust/service/IllustrationBizService.java
@@ -156,7 +156,7 @@ public class IllustrationBizService {
         illustration = objectMapper.convertValue(illustration, new TypeReference<Illustration>() {
         });
         if (illustration != null && illustration.getTags().size() > 0) {
-            String keywords = illustration.getTags().stream().parallel().filter(e -> !"".equals(e.getName())).limit(4).map(Tag::getName).reduce((x, y) -> x + "||" + y).get();
+            String keywords = illustration.getTags().stream().filter(e -> !"".equals(e.getName())).limit(4).map(Tag::getName).reduce((x, y) -> x + "||" + y).get();
             return searchService.searchByKeyword(keywords, pageSize, page, "original", null, null, null, null, null, 0, null, null, null, 5, illustId).thenApply(SearchResult::getIllustrations);
         }
         throw new BusinessException(HttpStatus.NOT_FOUND, "画作不存在");
