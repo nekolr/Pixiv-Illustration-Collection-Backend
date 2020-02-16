527b89e003528fe08c147de2e3bc4456cd6b3c0c
OysterQAQ <oysterqaq@gmail.com>
2020-02-16 15:56:50 +0800

    fixed: email check

--- src/main/java/dev/cheerfun/pixivic/biz/web/user/dto/SignUpDTO.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/user/dto/SignUpDTO.java
@@ -4,6 +4,7 @@ import dev.cheerfun.pixivic.basic.sensitive.annotation.SensitiveCheck;
 import dev.cheerfun.pixivic.biz.web.common.po.User;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -20,6 +21,7 @@ public class SignUpDTO {
     @Size(min = 2, max = 40)
     private String username;
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     @Size(min = 6, max = 20)
