701a211aab4868d8f4fdbf8defa42b99e1e262cc
OysterQAQ <oysterqaq@gmail.com>
2020-02-22 18:36:38 +0800

    fixed: rank order

--- src/main/java/dev/cheerfun/pixivic/biz/crawler/pixiv/service/IllustRankService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/crawler/pixiv/service/IllustRankService.java
@@ -47,14 +47,13 @@ public class IllustRankService {
     }
 
     private Rank getIllustrations(String mode, String date) throws InterruptedException {
-        ArrayList<Illustration> illustrations = new ArrayList<>(300);
+        ArrayList<Illustration> illustrations = new ArrayList<>();
         final CountDownLatch cd = new CountDownLatch(10);
         IntStream.range(0, 10).parallel().forEach(i -> getIllustrationsJson(mode, date, i).thenAccept(illustration -> {
             illustrations.addAll(illustration);
             cd.countDown();
         }));
         cd.await();
-        illustrations.trimToSize();
         String rankMode;
         switch (mode) {
             case "day_female":
