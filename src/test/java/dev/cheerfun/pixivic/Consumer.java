2d4cbc56460a197c6f685b03a4eac4f384e94156
OysterQAQ <oysterqaq@gmail.com>
2020-05-16 16:39:02 +0800

    refactor: refresh pixiv user pool
    重构账号池维护

--- src/test/java/dev/cheerfun/pixivic/Consumer.java
+++ src/test/java/dev/cheerfun/pixivic/Consumer.java
@@ -14,32 +14,7 @@ import java.util.concurrent.TimeoutException;
  */
 public class Consumer {
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
-        Address[] addresses = new Address[]{new Address("114.67.107.177", 5672)};
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUsername("root");
-        factory.setPassword("root");
-        //这里的连接方式与生产者的demo略有不同，注意辨别区别
-        Connection connection = factory.newConnection(addresses);//创建连接
-        final Channel channel = connection.createChannel();// 创建信道
-        channel.basicQos(64);//设置客户端最多接收未被ack的消息的个数
-        DefaultConsumer consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                System.out.println("recv message: " + new String(body));
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                channel.basicAck(envelope.getDeliveryTag(), false);
-            }
-        };
-
-        channel.basicConsume("QUEUE_DEMO", consumer);
-        //等待回调函数执行完毕之后，关闭资源
-        TimeUnit.SECONDS.sleep(100);
-        channel.close();
-        connection.close();
+        System.out.println(2495406 / 45000 / 2);
     }
 }
 
