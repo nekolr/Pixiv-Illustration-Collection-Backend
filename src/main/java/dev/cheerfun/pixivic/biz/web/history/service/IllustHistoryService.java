69af6e88f21e157ef82331a1aede82fe22a50bdc
OysterQAQ <oysterqaq@gmail.com>
2020-06-11 19:06:21 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/web/history/service/IllustHistoryService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/history/service/IllustHistoryService.java
@@ -78,7 +78,7 @@ public class IllustHistoryService {
         Cursor<byte[]> cursor = connection.scan(scanOptions);
         int i = 0;
         while (cursor.hasNext()) {
-            connection.zRemRangeByScore(cursor.next(), LocalDateTime.now().plusDays(-4).toEpochSecond(ZoneOffset.of("+8")), -Integer.MAX_VALUE);
+            connection.zRemRangeByScore(cursor.next(), LocalDateTime.now().plusDays(-3).toEpochSecond(ZoneOffset.of("+8")), -Integer.MAX_VALUE);
             i++;
         }
         illustHistoryMapper.deleteIllustHistory();
