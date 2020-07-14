b88c9ffbf74d9ab86eb6005d41d06521f4fb9ad6
OysterQAQ <oysterqaq@gmail.com>
2020-07-14 13:37:06 +0800

    feature: recommend module
    推荐模块

--- src/main/java/dev/cheerfun/pixivic/biz/recommend/service/IllustBookmarkRecommendService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/recommend/service/IllustBookmarkRecommendService.java
@@ -23,7 +23,6 @@ import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -108,9 +107,13 @@ public class IllustBookmarkRecommendService extends RecommendService {
     private void dealPerUser(List<Integer> u, Recommender recommender, Integer size) {
         u.forEach(e -> {
             try {
+                System.out.println("开始持久化用户：" + e + "的推荐列表");
                 List<RecommendedItem> recommend = recommender.recommend(e, 30 * size);
                 Set<ZSetOperations.TypedTuple<String>> typedTuples = recommend.stream().map(recommendedItem -> new DefaultTypedTuple<>(String.valueOf(recommendedItem.getItemID()), (double) recommendedItem.getValue())).collect(Collectors.toSet());
-                stringRedisTemplate.opsForZSet().add(RedisKeyConstant.USER_RECOMMEND_BOOKMARK_ILLUST + e, typedTuples);
+                typedTuples.forEach(t -> System.out.println(t.getValue() + ":" + t.getScore()));
+                if (typedTuples.size() > 0) {
+                    stringRedisTemplate.opsForZSet().add(RedisKeyConstant.USER_RECOMMEND_BOOKMARK_ILLUST + e, typedTuples);
+                }
             } catch (TasteException tasteException) {
                 tasteException.printStackTrace();
             }
