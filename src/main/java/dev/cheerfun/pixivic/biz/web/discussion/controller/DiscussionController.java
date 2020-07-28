5713df8c068479758e3842905fd2933a9d23c77e
OysterQAQ <oysterqaq@gmail.com>
2020-07-28 16:23:08 +0800

    fix

---
+++ src/main/java/dev/cheerfun/pixivic/biz/web/discussion/controller/DiscussionController.java
@@ -0,0 +1,17 @@
+package dev.cheerfun.pixivic.biz.web.discussion.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author OysterQAQ
+ * @version 1.0
+ * @date 2020/7/28 3:25 下午
+ * @description DiscussionController
+ */
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DiscussionController {
+
+}
---
+++ src/main/java/dev/cheerfun/pixivic/biz/web/discussion/service/DiscussionService.java
@@ -0,0 +1,22 @@
+package dev.cheerfun.pixivic.biz.web.discussion.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author OysterQAQ
+ * @version 1.0
+ * @date 2020/7/28 3:25 下午
+ * @description DiscussionService
+ */
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DiscussionService {
+    //新建
+    //点赞
+    //查看详情
+    //删除
+    //5分钟内更新
+
+}
