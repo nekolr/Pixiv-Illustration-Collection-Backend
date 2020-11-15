fd3063c6e97837110935c19b436ba450ac5ba188
OysterQAQ <oysterqaq@gmail.com>
2020-11-16 00:31:37 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/notify/customer/NotifyEventCustomer.java
+++ src/main/java/dev/cheerfun/pixivic/biz/notify/customer/NotifyEventCustomer.java
@@ -159,7 +159,7 @@ public abstract class NotifyEventCustomer {
             if (oldRemind.isPresent()) {
                 NotifyRemind notifyRemind = oldRemind.get();
                 //不能单纯add 需要考虑重复问题
-                if (notifyRemind.getActors().stream().noneMatch(a -> a.getUserId().compareTo(sendTo) == 0)) {
+                if (notifyRemind.getActors().stream().noneMatch(a -> a.getUserId().compareTo(sendTo) == 0 || a.getUserId().compareTo(event.getUserId()) == 0)) {
                     notifyRemind.getActors().add(Actor.castFromUser(userCommonService.queryUser(event.getUserId())));
                     notifyRemind.setCreateDate(event.getCreateDate());
                     notifyRemind.setActorCount(notifyRemind.getActors().size());
