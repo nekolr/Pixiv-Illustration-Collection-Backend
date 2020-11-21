a95ffe6871d76a1aa6254cbedeca1628255b8ee0
OysterQAQ <oysterqaq@gmail.com>
2020-11-21 11:12:54 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/web/collection/dto/CollectionDigest.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/collection/dto/CollectionDigest.java
@@ -1,28 +1,26 @@
 package dev.cheerfun.pixivic.biz.web.collection.dto;
 
 import dev.cheerfun.pixivic.biz.web.collection.po.Collection;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author OysterQAQ
  * @version 1.0
- * @date 2020/10/4 9:19 PM
+ * @date 2020/10/4 9 PM
  * @description ColletionDigest
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CollectionDigest {
     private Integer id;
     private String title;
+    private String caption;
 
     public static CollectionDigest castByCollection(Collection collection) {
-        return new CollectionDigest(collection.getId(), collection.getTitle());
+        return new CollectionDigest(collection.getId(), collection.getTitle(), collection.getCaption());
     }
 
-    public CollectionDigest(Integer id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
-    public CollectionDigest() {
-    }
 }
