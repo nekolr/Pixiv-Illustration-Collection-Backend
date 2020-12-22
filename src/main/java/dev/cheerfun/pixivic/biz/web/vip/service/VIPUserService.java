cd61c7606732feadca6b7cb8e041a54751a8d88f
OysterQAQ <oysterqaq@gmail.com>
2020-12-22 21:30:34 +0800

    调整会员

--- src/main/java/dev/cheerfun/pixivic/biz/web/vip/service/VIPUserService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/vip/service/VIPUserService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -94,7 +95,11 @@ public class VIPUserService {
     //增加一个会员试用表
     @Cacheable(value = "CanParticipateActivity", key = "#userId+'-'+#activityName")
     public Boolean checkCanParticipateActivity(Integer userId, String activityName) {
-        return VIPMapper.checkActivity(userId, activityName) == null;
+        //用户id对100取余 定义一个变量 为当前日期-2020-12-22的天数
+        if (userId % 100 < 3 * Period.between(LocalDate.of(2020, 12, 22), LocalDate.now().plusDays(1)).getDays()) {
+            return VIPMapper.checkActivity(userId, activityName) == null;
+        }
+        return false;
     }
 
     @CacheEvict(value = "CanParticipateActivity", key = "#userId+'-'+#activityName")
--- src/test/java/dev/cheerfun/pixivic/Consumer.java
+++ src/test/java/dev/cheerfun/pixivic/Consumer.java
@@ -18,7 +18,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,59 +36,8 @@ import static org.opencv.imgproc.Imgproc.COLOR_BGR2RGB;
  */
 public class Consumer {
     public static void main(String[] args) throws Exception {
-     /*   NativeImageLoader loader = new NativeImageLoader(224, 224, 3, new ColorConversionTransform(COLOR_BGR2RGB));
 
-        INDArray indArray = loader.asMatrix(new File("/Users/oysterqaq/Desktop/3.jpg"), false);
-        System.out.println(Arrays.toString(indArray.shape()));
-        System.out.println(indArray);
-        DataNormalization scaler = new VGG16ImagePreProcessor();
-        // scaler.transform(indArray);
-        INDArray mean = Nd4j.create(new double[]{103.939, 116.779, 123.68}).castTo(DataType.FLOAT);
-        Nd4j.getExecutioner().execAndReturn(new BroadcastSubOp(indArray.dup(), mean, indArray, 3));
-        System.out.println(indArray);
-        HttpClient httpClient = HttpClient.newHttpClient();
-        URI uri = URI.create("http://manjaro:8501/v1/models/vgg:predict");
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(uri).POST(HttpRequest.BodyPublishers.ofString("{\"instances\":"+indArray.toStringFull()+"}")).build();
-          String body = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
-       System.out.println(body);
-        Predictions predictions = new ObjectMapper().readValue(body, Predictions.class);
-        System.out.println(predictions.predictions[0].length);
-*/
-
-        NativeImageLoader loader = new NativeImageLoader(512, 512, 3, new ColorConversionTransform(COLOR_BGR2RGB));
-
-        INDArray indArray = loader.asMatrix(new File("/Users/oysterqaq/Desktop/test.jpg"), false).div(255);
-        System.out.println(Arrays.toString(indArray.shape()));
-        // DataNormalization scaler = new VGG16ImagePreProcessor();
-        // scaler.transform(indArray);
-        // INDArray mean = Nd4j.create(new double[]{103.939, 116.779, 123.68}).castTo(DataType.FLOAT);
-        //  Nd4j.getExecutioner().execAndReturn(new BroadcastSubOp(indArray.dup(), mean, indArray, 3));
-        System.out.println(indArray);
-        HttpClient httpClient = HttpClient.newHttpClient();
-        String body = null;
-        // for (int i = 0; i < 10; i++) {
-            System.out.println(LocalDateTime.now());
-            URI uri = URI.create("http://manjaro:8501/v1/models/deepdanbooru:predict");
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(uri).POST(HttpRequest.BodyPublishers.ofString("{\"instances\":" + indArray.toStringFull() + "}")).build();
-            body = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
-            System.out.println(LocalDateTime.now());
-
-        //   }
-
-        System.out.println(body);
-        Predictions predictions = new ObjectMapper().readValue(body, Predictions.class);
-        List<String> strings = Files.readLines(new File("/Users/oysterqaq/Desktop/deepdanbooru-v4-20200814-sgd-e30/tags.txt"), Charset.defaultCharset());
-        for (Float[] prediction : predictions.predictions) {
-            IntStream.range(0, prediction.length).forEach(e -> {
-                if (prediction[e] > 0.6) {
-                    System.out.println(strings.get(e) + ":" + prediction[e]);
-                }
-            });
-            ///  System.out.println(Arrays.stream(prediction).parallel().filter(e -> e > 0.6f).collect(Collectors.toList()));
-        }
-        System.out.println(predictions.predictions[0].length);
+        System.out.println(Period.between(LocalDate.of(2020, 12, 22), LocalDate.of(2020, 12, 22)).getDays());
 
     }
 
