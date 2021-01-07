9e0834a10cb271e6d75eda7955b50ac1593ec030
OysterQAQ <oysterqaq@gmail.com>
2021-01-07 17:58:07 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/ad/service/AdvertisementService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/ad/service/AdvertisementService.java
@@ -72,14 +72,14 @@ public class AdvertisementService {
         //使用布隆过滤器查看是否投放过一次
         if (bloomFilter.mightContain(identification)) {
             //如果投放过 以一个较低的随机来投放
-            if (userId == null ? isAdd < 200 : isAdd < ((int) AppContext.get().get(AuthConstant.PERMISSION_LEVEL) < PermissionLevel.VIP ? 100 : 85)) {
+            if (userId == null ? isAdd < 160 : isAdd < ((int) AppContext.get().get(AuthConstant.PERMISSION_LEVEL) < PermissionLevel.VIP ? 90 : 85)) {
                 //优化成取余
                 Advertisement advertisement = advertisementList.get(randomList.get(isAdd % randomList.size()));
                 return Collections.singletonList(advertisement);
             }
         } else {
             //如果没投放过 以一个较高的随机来投放 且将每个广告主的广告都投放一次
-            if (isAdd < 500) {
+            if (isAdd < 600) {
                 List<Advertisement> advertisementList = new ArrayList<>(advertisementMap.size());
                 advertisementMap.forEach((k, v) -> {
                     advertisementList.add(v.get(0));
