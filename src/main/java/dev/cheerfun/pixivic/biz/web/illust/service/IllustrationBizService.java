e7ea0c9d08950ea7880e0056318c53b0bbc87f22
OysterQAQ <oysterqaq@gmail.com>
2021-01-22 11:10:47 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/web/illust/service/IllustrationBizService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/illust/service/IllustrationBizService.java
@@ -126,7 +126,7 @@ public class IllustrationBizService {
         Illustration illustration = illustrationBizMapper.queryIllustrationByIllustId(illustId);
         if (illustration == null) {
             log.info("画作：" + illustId + "不存在，加入队列等待爬取");
-            waitForPullIllustQueue.offer(illustId);
+            //waitForPullIllustQueue.offer(illustId);
         }
         if (illustration.getSanityLevel() > 3 || illustration.getTotalBookmarks() < 300) {
             return null;
