80942c78092a223818678dcd6e7f40b2fca8b4c7
OysterQAQ <oysterqaq@gmail.com>
2021-02-04 15:35:20 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/web/comment/controller/CommentController.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/comment/controller/CommentController.java
@@ -56,7 +56,7 @@ public class CommentController {
     @GetMapping("/{commentAppType}/{commentAppId}/comments")
     @PermissionRequired(PermissionLevel.ANONYMOUS)
     public ResponseEntity<Result<List<Comment>>> pullComment(@PathVariable String commentAppType, @PathVariable int commentAppId, @RequestHeader(value = AuthConstant.AUTHORIZATION, required = false) String token, @RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer pageSize) {
-        return ResponseEntity.ok().body(new Result<>("拉取评论成功", 0, null));
+        return ResponseEntity.ok().body(new Result<>("拉取评论成功", commentService.queryTopCommentCount(commentAppType, commentAppId), commentService.pullComment(commentAppType, commentAppId, page, pageSize)));
     }
 
     @GetMapping("/{commentAppType}/{commentAppId}/topCommentCount")
