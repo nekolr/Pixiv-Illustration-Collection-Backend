2f7d0928ec1211be33df6b24ce5b8ce79d058753
OysterQAQ <oysterqaq@gmail.com>
2021-02-09 17:55:50 +0800

    fix

--- src/main/java/dev/cheerfun/pixivic/biz/web/user/service/CommonService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/user/service/CommonService.java
@@ -229,7 +229,7 @@ public class CommonService {
         return user.getIsBindQQ();
     }
 
-    @Cacheable(value = "users", key = "#userId")
+    @Cacheable(value = "users", key = "'user_'.concat(#a0)")
     public User queryUser(Integer userId) {
         User user = userMapper.queryUserByUserId(userId);
         if (user == null) {
