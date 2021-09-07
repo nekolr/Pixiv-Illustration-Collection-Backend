9d7bc0b9c7525ed3821393dc52cbf3bd93d20c19
OysterQAQ <oysterqaq@gmail.com>
2021-09-07 10:31:08 +0800

    feature: discuss token generate

--- src/main/java/dev/cheerfun/pixivic/biz/web/user/service/CommonService.java
+++ src/main/java/dev/cheerfun/pixivic/biz/web/user/service/CommonService.java
@@ -454,7 +454,7 @@ public class CommonService {
             //自动注册
             User user = queryUser(userId);
             HttpRequest register = HttpRequest.newBuilder()
-                    .uri(URI.create("https://discuss.sharemoe.net/register")).POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"" + user.getUsername() + "\",\"email\":\"" + user.getEmail() + "\",\"token\":\"" + token + "\"}"))
+                    .uri(URI.create("https://discuss.sharemoe.net/register")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"" + user.getUsername() + "\",\"email\":\"" + user.getEmail() + "\",\"token\":\"" + token + "\"}"))
                     .headers("Cookie", flarumSession)
                     .build();
             HttpResponse<String> registerResponse = httpClient.send(register, HttpResponse.BodyHandlers.ofString());
@@ -463,7 +463,7 @@ public class CommonService {
                 log.info(registerResponse.body());
                 log.info("用户论坛用户名重复，进行重新注册");
                 register = HttpRequest.newBuilder()
-                        .uri(URI.create("https://discuss.sharemoe.net/register")).POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"" + user.getUsername().substring(0, 2) + userId + "\",\"email\":\"" + user.getEmail() + "\",\"token\":\"" + token + "\"}"))
+                        .uri(URI.create("https://discuss.sharemoe.net/register")).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"" + user.getUsername().substring(0, 2) + userId + "\",\"email\":\"" + user.getEmail() + "\",\"token\":\"" + token + "\"}"))
                         .headers("Cookie", flarumSession)
                         .build();
                 registerResponse = httpClient.send(register, HttpResponse.BodyHandlers.ofString());
